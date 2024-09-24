0ca3296f7420ecc3132dd4002cc69bf19475db27
Eerie <sincerely.eerie@gmail.com>
2024-09-24 09:43:14 -0700

    Added ADD!

--- main.c
+++ main.c
@@ -10,63 +10,61 @@ typedef unsigned char Uint8;
 typedef unsigned short int Uint16;
 
 // microcode
-enum opcodes {
-	NOOP,
-	PUSH,
-  POP,
-	HLT = 255
-};
+enum opcodes { NOOP, PUSH, POP, ADD, SUB, MUL, DIV, MOD, HLT = 255 };
 
 typedef struct {
-	Uint8 data[256];
-	Uint8 sp;
+  Uint8 data[256];
+  Uint8 sp;
 } stack;
 
 typedef struct {
-	Uint8 prog[65536];
-	Uint16 ip;
-	stack dst, rst;
+  Uint8 prog[65536];
+  Uint16 ip;
+  stack dst, rst;
 } VM;
 
 void push(stack *st, Uint8 val) {
-	st->sp++;
-	st->data[st->sp] = val;
+  st->sp++;
+  st->data[st->sp] = val;
 }
 
-Uint8 pop(stack *st) {
-	return st->data[st->sp--];
-}
+Uint8 pop(stack *st) { return st->data[st->sp--]; }
 
 void dispatch(VM *vm) {
-	switch(vm->prog[vm->ip]) {
-		case NOOP:
-			// Do nothing
-			break;
-		case PUSH:
-			vm->ip++;
-			push(&vm->dst, vm->prog[vm->ip]);
-			break;
-		case POP:
-			// temp for pop test
-			printf("Popped: %x", pop(&vm->dst));
-			break;
-		case HLT:
-			// Crash and burn
-			fprintf(stderr, "HOT TAMALE!\n");
-			exit(255);
-			break;
-	}
+  Uint8 a, b, c;
+  switch (vm->prog[vm->ip]) {
+  case NOOP:
+    // Do nothing
+    break;
+  case PUSH:
+    vm->ip++;
+    push(&vm->dst, vm->prog[vm->ip]);
+    break;
+  case POP:
+    // temp for pop test
+    printf("Popped: %x", pop(&vm->dst));
+    break;
+  case ADD:
+    a = pop(&vm->dst);
+    b = pop(&vm->dst);
+    push(&vm->dst, a + b);
+    break;
+  case HLT:
+    // Crash and burn
+    fprintf(stderr, "EXECUTION CEASED\n");
+    exit(255);
+    break;
+  }
 }
 
-void run_vm (VM *vm) {
-	while (vm->ip < PROG_MAX) {
-		dispatch(vm);
-		vm->ip++;
-	}
+void run_vm(VM *vm) {
+  while (vm->ip < PROG_MAX) {
+    dispatch(vm);
+    vm->ip++;
+  }
 }
 
 int main(void) {
-	VM vm = {.prog={PUSH, 0x69, POP}};
-	run_vm(&vm);
+  VM vm = {.prog = {PUSH, 0x69, PUSH, 0x02, ADD, POP}};
+  run_vm(&vm);
 }
-
